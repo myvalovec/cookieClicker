5becf1c43678007596b08f4dbd75798b68178797
myvalovec <myvalovec6@gmail.com>
2025-05-13 20:18:39 +0000

    May 13, 2025, 10:18 PM

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function klikniNaCookie() {
     body++;
     document.getElementById("body").innerText = body;
 }
+
+function naPana() {
+    document.getElementById("cookie").src = "./orbazky/ministr.jpg"
+}
+
+function spink() {
+    document.getElementById("cookie").src = "./orbazky/spink.jpg"
+}

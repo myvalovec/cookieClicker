7e8fbd4b8fb0beccf569d6d824bc496595125464
myvalovec <myvalovec6@gmail.com>
2025-05-19 12:59:01 +0000

    May 19, 2025, 2:59 PM

--- script.js
+++ script.js
@@ -2,45 +2,6 @@ let body = 0;
 function klikniNaCookie() {
     body++;
     document.getElementById("body").innerText = body;
-    const lp = document.getElementById('lp');
-
-const square = document.createElement('div');
-square.style.width = '200px';
-square.style.height = '80px';
-square.style.border = '1px solid black';
-square.style.display = 'flex';
-square.style.justifyContent = 'space-between';
-square.style.alignItems = 'center';
-square.style.padding = '10px';
-square.style.cursor = 'pointer';
-
-// Texty
-const texts = document.createElement('div');
-texts.style.textAlign = 'left';
-
-texts.innerHTML = `
-  <div>Text 1</div>
-  <div>Text 2</div>
-  <div>Text 3</div>
-`;
-
-// Obrázek
-const img = document.createElement('img');
-img.src = './obrazky/vojtik.jpg';
-img.style.width = '20px';
-img.style.height = '50px';
-
-// Přidání do čtverce
-square.appendChild(img);
-square.appendChild(texts);
-
-
-// Kliknutí
-square.onclick = () => alert('Kliknuto!');
-
-// Přidání do stránky
-lp.appendChild(square);
-
 }
 
 function naPana() {

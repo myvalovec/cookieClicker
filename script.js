9067921c1ac2ea4ec2bd770c7c7df908b03e5bbb
myvalovec <myvalovec6@gmail.com>
2025-05-19 19:24:27 +0000

    May 19, 2025, 9:24 PM

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function naPana() {
 function spink() {
     document.getElementById("cookie").src = "./obrazky/spink.jpg"
 }
+
 function createSquare(text1, text2, text3, imageUrl) {
     const container = document.getElementById('lp');
 
@@ -31,19 +32,16 @@ function createSquare(text1, text2, text3, imageUrl) {
     textPart.style.paddingRight = '10px';
 
     const t1 = document.createElement('div');
-    const t2 = document.createElement('div');
-    t2.style.fontSize = "10px";
+    t1.style.fontSize = "30px";
     const t3 = document.createElement('div');
     t3.style.fontWeight = "bold";
+    t3.style.fontSize = "20px";
     t1.innerHTML = text1;
-    t2.innerHTML = text2;
     t3.innerHTML = text3;
 
     textPart.appendChild(t1);
-    textPart.appendChild(t2);
     textPart.appendChild(t3);
 
-  // Pravá část s obrázkem (menší)
     const imagePart = document.createElement('div');
     imagePart.style.flex = '1';
     imagePart.style.display = 'flex';
@@ -56,12 +54,10 @@ function createSquare(text1, text2, text3, imageUrl) {
     img.style.height = '50px';
     imagePart.appendChild(img);
 
-  // Spojení částí
     square.appendChild(imagePart);
     square.appendChild(textPart);
     
 
-  // Kliknutí
     square.onclick = () => alert('Kliknuto!');
 
     container.appendChild(square);
